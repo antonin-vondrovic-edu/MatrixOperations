4d1a37c3279de32b52860b140aa27d68f0e65d97
Antonín Vondrovic <antonin.vondrovic@educanet.cz>
2021-02-12 15:29:52 +0100

    matrixtest finished

--- src/test/java/cz/educanet/tranformations/MatrixTest.java
+++ src/test/java/cz/educanet/tranformations/MatrixTest.java
@@ -63,22 +63,88 @@ public class MatrixTest {
 
     @Test
     public void times() {
+        IMatrix times = a.times(b);
+
+        System.out.println();
+        System.out.println("Times A*B:");
+        for (int i = 0; i < times.getRows(); i++) {
+            for (int j = 0; j < times.getColumns(); j++) {
+                System.out.print(times.get(i, j) + " ");
+            }
+            System.out.println();
+        }
 
     }
 
     @Test
     public void timesScalar() {
+        int forScalar = 2;
+        IMatrix timesScalarA = a.times(forScalar);
+        IMatrix timesScalarB = b.times(forScalar);
+        IMatrix timesScalarC = c.times(forScalar);
+        IMatrix timesScalarD = d.times(forScalar);
 
+        System.out.println();
+        System.out.println("Times Scalar A*" + forScalar + ":");
+        for (int i = 0; i < timesScalarA.getRows(); i++) {
+            for (int j = 0; j < timesScalarA.getColumns(); j++) {
+                System.out.print(timesScalarA.get(i, j) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Times Scalar B*" + forScalar + ":");
+        for (int i = 0; i < timesScalarB.getRows(); i++) {
+            for (int j = 0; j < timesScalarB.getColumns(); j++) {
+                System.out.print(timesScalarB.get(i, j) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Times Scalar C*" + forScalar + ":");
+        for (int i = 0; i < timesScalarC.getRows(); i++) {
+            for (int j = 0; j < timesScalarC.getColumns(); j++) {
+                System.out.print(timesScalarC.get(i, j) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Times Scalar D*" + forScalar + ":");
+        for (int i = 0; i < timesScalarD.getRows(); i++) {
+            for (int j = 0; j < timesScalarD.getColumns(); j++) {
+                System.out.print(timesScalarD.get(i, j) + " ");
+            }
+            System.out.println();
+        }
     }
 
     @Test
     public void add() {
+        IMatrix adding = a.add(b);
 
+        System.out.println();
+        System.out.println("Addition A+B:");
+        for (int i = 0; i < adding.getRows(); i++) {
+            for (int j = 0; j < adding.getColumns(); j++) {
+                System.out.print(adding.get(i,j) + " ");
+            }
+            System.out.println();
+        }
     }
 
     @Test
     public void get() {
+        double getA = a.get(0, 0);
+        double getB = b.get(0, 0);
+        double getC = c.get(0, 0);
+        double getD = d.get(0, 0);
 
+        System.out.println();
+        System.out.println("getA0: " + getA + " getB0: " + getB);
+        System.out.println("getC0: " + getC + " getD0: " + getD);
     }
 
     @Test
